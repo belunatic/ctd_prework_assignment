63a94f46e95191dbb92d0cafc26565ff657074f5
belunatic <abelluvyou@yahoo.com>
2026-04-04 15:39:16 -0700

    add capability to fetch the data and add it to the client side

--- js/people.js
+++ js/people.js
@@ -12,13 +12,62 @@ const fetchStarWarCharacter = async () => {
 	}
 };
 
-document.addEventListener("DOMContentLoaded", async () => {
-	await fetchStarWarCharacter();
-	const characterList = document.getElementById("character-list");
+//fetch character profile
+const fetchStartWarCharacterProfile = async (id) => {
+	try {
+		const res = await fetch(`https://www.swapi.tech/api/people/${id}`);
+		const data = await res.json();
+		return data.result.properties;
+	} catch (error) {
+		console.log("something went wrong", error.message);
+	}
+};
 
-	character.forEach((char) => {
-		const li = document.createElement("li");
-		li.textContent = char.name;
-		characterList.appendChild(li);
+document.addEventListener("DOMContentLoaded", async () => {
+	//fetch characters
+	await fetchStarWarCharacter();
+	//grab the div container
+	const sectionCard = document.getElementById("characterListDiv");
+
+	character.forEach(async (char) => {
+		//create a div and add a class to it
+		const characterCards = document.createElement("div");
+		characterCards.classList.add("character-cards");
+
+		//attach the character name to div
+		const characterName = document.createElement("h2");
+		characterName.textContent = char.name;
+
+		//get character profile info
+		const characterProfile = await fetchStartWarCharacterProfile(char.uid);
+		//UL
+		//character profile ul
+		const characterProfileUl = document.createElement("ul");
+		//LI
+		//character birth year
+		const characterBirthYear = document.createElement("li");
+		characterBirthYear.textContent = `Birth Year: ${characterProfile.birth_year}`;
+		characterProfileUl.appendChild(characterBirthYear);
+		//character gender
+		const characterGender = document.createElement("li");
+		characterGender.textContent = `Gender: ${characterProfile.gender}`;
+		characterProfileUl.appendChild(characterGender);
+		//character films
+		const characterFilms = document.createElement("li");
+		const filmsList = characterProfile.films.join(", ");
+		characterFilms.textContent = `Films: ${filmsList}`;
+		characterProfileUl.appendChild(characterFilms);
+		//character learn more url
+		const characterLearnMore = document.createElement("li");
+		characterLearnMore.innerHTML = `<a href="${characterProfile.url}" target="_blank">Learn More</a>`;
+		characterProfileUl.appendChild(characterLearnMore);
+		//APPEND TO UL
+		//append the ul to the character card
+		characterCards.appendChild(characterProfileUl);
+
+		//APPEND TO DIV
+		characterCards.appendChild(characterName);
+		characterCards.appendChild(characterProfileUl);
+		sectionCard.appendChild(characterCards);
 	});
 });
